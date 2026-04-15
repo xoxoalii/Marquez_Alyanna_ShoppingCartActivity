46635880871980cd6b0366d965aeae0895fae846
xoxoalii <alyannamarquez1@gmail.com>
2026-04-16 06:01:19 +0800

    Implement Product class for shopping cart
    Added Product class with properties and display method.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,18 @@
 using System;
 
+class Product
+{
+    public int Id;
+    public string Name;
+    public double Price;
+    public int RemainingStock;
+
+    public void DisplayProduct()
+    {
+        Console.WriteLine($"{Id}. {Name} - ₱{Price} (Stock: {RemainingStock})");
+    }
+}
+
 class Program
 {
     static void Main()
