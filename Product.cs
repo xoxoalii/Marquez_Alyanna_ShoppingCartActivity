c93ab7093c4b88c7235a30ffd96c06ff892c353f
xoxoalii <alyannamarquez1@gmail.com>
2026-05-01 12:07:00 +0800

    Add Product class with properties and methods

---
+++ Product.cs
@@ -0,0 +1,29 @@
+using System;
+
+class Product
+{
+    public int Id;
+    public string Name;
+    public double Price;
+    public int RemainingStock;
+
+    public void DisplayProduct()
+    {
+        Console.WriteLine($"{Id}. {Name} - ₱{Price} (Stock: {RemainingStock})");
+    }
+
+    public double GetItemTotal(int quantity)
+    {
+        return Price * quantity;
+    }
+
+    public bool HasEnoughStock(int quantity)
+    {
+        return quantity <= RemainingStock;
+    }
+
+    public void DeductStock(int quantity)
+    {
+        RemainingStock -= quantity;
+    }
+}
