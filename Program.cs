4e2dc36f9f9e73a56771c76831e190cf9da79d62
xoxoalii <alyannamarquez1@gmail.com>
2026-05-01 12:07:43 +0800

    Delete Product and CartItem classes
    Removed Product and CartItem classes along with their methods.

--- Program.cs
+++ Program.cs
@@ -1,45 +1,5 @@
 using System;
 
-class Product
-{
-    public int Id;
-    public string Name;
-    public double Price;
-    public int RemainingStock;
-
-    public void DisplayProduct()
-    {
-        Console.WriteLine($"{Id}. {Name} - ₱{Price} (Stock: {RemainingStock})");
-    }
-    
-    public double GetItemTotal(int quantity)
-    {
-        return Price * quantity;
-    }
-
-    public bool HasEnoughStock(int quantity)
-    {
-        return quantity <= RemainingStock;
-    }
-
-    public void DeductStock(int quantity)
-    {
-        RemainingStock -= quantity;
-    }
-}
-
-class CartItem
-{
-    public Product Product;
-    public int Quantity;
-    public double Subtotal;
-
-    public void UpdateSubtotal()
-    {
-        Subtotal = Product.Price * Quantity;
-    }
-}
-
 class Program
 {
     static void Main()
